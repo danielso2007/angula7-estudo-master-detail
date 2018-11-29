287c884d4d33e1b549e3206d48a0f5f710d79c38
danielso2007 <danielso2007@gmail.com>
2018-11-29 03:13:51 -0200

    feat: Otimizando a listagem de Lançamentos e Categorias.

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,40 +1,17 @@
 import { CategoryService } from './../shared/category.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Category } from '../shared/category.model';
+import { BaseListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent extends BaseListComponent<Category> {
 
-  categories: Category[] = null;
-  action = false;
-
-  constructor(private categoryService: CategoryService) { }
-
-  ngOnInit() {
-    this.categoryService.getAll().subscribe(
-      categories => this.categories = categories,
-      error => alert('Erro ao criar lista')
-    );
-  }
-
-  deleteCategory(category: Category): void {
-    this.action = true;
-    const mustDelete = confirm('Deseja realmente excluir este item?');
-    if (mustDelete) {
-      this.categoryService.delete(category.id).subscribe(
-        () => {
-          this.categories = this.categories.filter(element => element !== category);
-          this.action = false;
-        },
-        error => { alert('Erro ao deletar categoria'); this.action = false; }
-      );
-    } else {
-      this.action = false;
-    }
+  constructor(protected categoryService: CategoryService) {
+    super(categoryService);
   }
 
 }
